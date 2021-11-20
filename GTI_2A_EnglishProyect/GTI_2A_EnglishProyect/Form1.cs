ac6100413666c972f9dc4e99289f7e49666ac9b5
Juan <tostyfis360@gmail.com>
2021-11-20 13:19:23 +0100

    Project created 20/11/2021

---
+++ GTI_2A_EnglishProyect/GTI_2A_EnglishProyect/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GTI_2A_EnglishProyect
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
