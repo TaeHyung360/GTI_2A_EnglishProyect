2f3e5c5ab3f66c5ad71e8d63151549f788ebdd09
Juan <tostyfis360@gmail.com>
2021-11-20 18:06:14 +0100

    Login created 20/11/2021
    Password: admin

--- GTI_2A_EnglishProyect/GTI_2A_EnglishProyect/Form1.Designer.cs
+++ GTI_2A_EnglishProyect/GTI_2A_EnglishProyect/Form1.Designer.cs
@@ -29,13 +29,95 @@ namespace GTI_2A_EnglishProyect
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textUser = new System.Windows.Forms.TextBox();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.titleForm1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(19, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(297, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textUser
+            // 
+            this.textUser.Location = new System.Drawing.Point(91, 109);
+            this.textUser.Name = "textUser";
+            this.textUser.Size = new System.Drawing.Size(225, 20);
+            this.textUser.TabIndex = 1;
+            // 
+            // textPassword
+            // 
+            this.textPassword.Location = new System.Drawing.Point(91, 155);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.Size = new System.Drawing.Size(225, 20);
+            this.textPassword.TabIndex = 2;
+            // 
+            // titleForm1
+            // 
+            this.titleForm1.AutoSize = true;
+            this.titleForm1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleForm1.Location = new System.Drawing.Point(12, 28);
+            this.titleForm1.Name = "titleForm1";
+            this.titleForm1.Size = new System.Drawing.Size(171, 39);
+            this.titleForm1.TabIndex = 3;
+            this.titleForm1.Text = "User login";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "User";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(347, 320);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.titleForm1);
+            this.Controls.Add(this.textPassword);
+            this.Controls.Add(this.textUser);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textUser;
+        private System.Windows.Forms.TextBox textPassword;
+        private System.Windows.Forms.Label titleForm1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- GTI_2A_EnglishProyect/GTI_2A_EnglishProyect/Form1.cs
+++ GTI_2A_EnglishProyect/GTI_2A_EnglishProyect/Form1.cs
@@ -12,9 +12,54 @@ namespace GTI_2A_EnglishProyect
 {
     public partial class Form1 : Form
     {
+
+        //Intentos para cerrar sesion
+
+        private int attempts = 3;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string valuePassword = textPassword.Text;
+
+            if (valuePassword.Equals("admin"))
+            {
+                Form2 secondaryWindow = new Form2();
+
+                //Oculta este formulario
+                this.Hide();
+                //Abre el Form2
+                secondaryWindow.ShowDialog();
+                //Cierra el formulario actual
+                this.Close();
+
+            }else if(valuePassword.Length == 0)
+            {
+
+                MessageBox.Show("The password field is required");
+
+            }
+            else
+            {
+                attempts--;
+
+                if(attempts == 0)
+                {
+
+                    MessageBox.Show("You have exceeded 3 attempts, please try again later. ");
+
+                    this.Close();
+
+                }
+
+                MessageBox.Show("The password is wrong\n" + "Your remaining attempts are: " + attempts.ToString());
+
+            }
         }
     }
 }
